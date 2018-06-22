9e5cc1336975820ac2e201b8c462c50109fe4dd6
Matthew Cain <yooksi@zoho.com>
2018-06-22 09:51:34 +0200

    Correct batch typo

--- create_release.bat
+++ create_release.bat
@@ -1,3 +1,3 @@
 @echo off
 set /p id="Enter release name: "
-7z a -mm=Deflate -mfb=258 -mpass=15 -r %cd%\Public\Release\%id%.zip %cd%\Workspace\Assets*
+7z a -mm=Deflate -mfb=258 -mpass=15 -r %cd%\Public\Release\%id%.zip %cd%\Workspace\Assets\*
